d1da8f9a61f2e2503a7ed4a4c8cd401c002ffc5e
Dennis Stangl <d.stangl@oth-aw.de>
2021-06-02 13:42:59 +0200

    Initial C# Backend commit
    Former-commit-id: 861cd4f03758e56b72a1ee0d756941d2bb069f02

---
+++ src/backend/Backend.API/Controllers/GameDataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameDataController : ControllerBase
+    {
+        [HttpGet]
+        public string GetGameData(int id)
+        {
+            return "ID: " + id;
+        }
+    }
+}
---
+++ src/backend/Backend.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Backend.Domain
+{
+    public class Class1
+    {
+    }
+}
