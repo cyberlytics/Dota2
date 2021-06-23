b0a3b38fed5fae8649a3277d516caee7b7e0a053
Dennis Stangl <d.stangl@oth-aw.de>
2021-06-23 13:54:15 +0200

    Started implementing JupyterService.cs
    Former-commit-id: 0b6f25c2154a88d07298ebaa087155ffca2baf5b

---
+++ src/images/backend/Backend.API/Controllers/DatabaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Domain.Models;
+using Backend.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly IMatchRepository _matchRepository;
+
+        public DatabaseController(IMatchRepository matchRepository)
+        {
+            _matchRepository = matchRepository;
+        }
+        
+        [HttpGet]
+        [Route("get")]
+        public Match GetAll()
+        {
+            return _matchRepository.Get().FirstOrDefault();
+        }
+    }
+}
---
+++ src/images/backend/Backend.Domain/Services/IJupyterService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace Backend.Domain.Services
+{
+    public interface IJupyterService
+    {
+        public Task<HttpStatusCode> WriteMatchAsync(long matchId);
+    }
+}
