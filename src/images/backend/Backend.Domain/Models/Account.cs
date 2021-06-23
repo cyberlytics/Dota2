5236f68b6056a52e89ebaeabf14c5a6d6d93a48b
Maximilian Zuber <m.zuber@oth-aw.de>
2021-06-23 11:06:11 +0200

    Weitere Anpassungen an OpenDota Schnittstelle - Rückgabewerte der Controllerfunktionen in geändert - Klasse MatchId gelöscht - Neue Funktion: Finde Steam32 ID eines Accounts anhand des Usernamen heraus - Debugzähler entfernt
    Former-commit-id: 7901c3ee0aabd11354db7ee080bafd2720faa76d

---
+++ src/images/backend/Backend.Domain/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+#pragma warning disable IDE1006 // Benennungsstile
+
+namespace Backend.Domain.Models
+{
+    public class Account
+    {
+        ///<summary>
+        ///Steam32 ID
+        ///</summary>
+        public long account_id { get; set; }
+
+        ///<summary>
+        ///Nickname
+        ///</summary>
+        public string personaname { get; set; }
+
+        ///<summary>
+        ///Avatar URL
+        ///</summary>
+        public string avatarfull { get; set; }
+
+        ///<summary>
+        ///last_match_time. May not be present or null.
+        ///</summary>
+        public DateTime last_match_time { get; set; }
+
+        ///<summary>
+        ///similarity
+        ///</summary>
+        public float similarity { get; set; }
+    }
+}
--- src/images/backend/Backend.Domain/Services/IOpenDotaService.cs
+++ src/images/backend/Backend.Domain/Services/IOpenDotaService.cs
@@ -6,10 +6,14 @@ namespace Backend.Domain.Services
 {
     public interface IOpenDotaService
     {
-        Task<string> FetchNewMatches(int number = 1);
+        Task<List<long>> FetchNewMatches(int number = 1);
 
-        Task<string> FetchNewMatchesAndParse(int number = 1);
+        Task<List<long>> FetchNewMatchesAndParse(int number = 1);
 
-        Task<string> FetchAllMatchesForPlayer(long steam32Id, int limit = 100);
+        Task<List<long>> FetchAllMatchesForPlayer(long steam32Id, int limit = 100);
+
+        Task<List<long>> FetchRecentMatchesForPlayer(long steam32Id);
+
+        Task<long> GetSteamIdByPersonaName(string name);
     }
 }
